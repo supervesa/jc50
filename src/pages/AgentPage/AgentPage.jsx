28489fd52a689e8185b8508c2a1e7cee5cd8242b
supervesa <52207683+supervesa@users.noreply.github.com>
2025-12-09 09:50:28 +0200

    agent mobilefix backback

--- src/pages/AgentPage/AgentPage.jsx
+++ src/pages/AgentPage/AgentPage.jsx
@@ -57,7 +57,7 @@ const AgentPage = () => {
   if (!guestId || !identity) return <div className="ap-error">VIRHEELLINEN ID</div>;
 
   return (
-    <div id="agent-page" className="ap-container">
+    <div className="ap-container">
       
       {/* 1. HEADER (Näkyy aina ylhäällä) */}
       <AgentHeader 
